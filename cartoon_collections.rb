441a785cf0cf95dc9bf20ad76a4d81032e506001
Ro Flo <iroflood@msn.com>
2019-02-16 21:51:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,15 +19,12 @@ def long_planeteer_calls(array)
    value == nil ? false : true
  end		 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+
 
 def find_the_cheese(array)
-   cheese_types = ["cheddar", "gouda", "camembert"]		   cheese_types = ["cheddar", "gouda", "camembert"]
+   cheese_types = ["cheddar", "gouda", "camembert"]
    cheese_types.each do |cheese|
      return cheese if array.include?(cheese) == true
    end
    nil
- end		 end
+ end		 
